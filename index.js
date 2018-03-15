107455ea7ecc422424099fdcbc6a9e7e30c16d64
Curtis Wakley <curtis.wakley@gmail.com>
2018-03-15 17:55:25 -0600

    I am an idiot.

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var fs = require('fs');
-var youtubedl = require('youtube-dl');
-var video = youtubedl('https://www.youtube.com/watch?v=FZUcpVmEHuk', ['--format=18'], {cwd: __dirname});
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
